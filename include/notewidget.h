7f351b90dd81a3d22942822ca040e91c3ee745a7
Marcus Haehnel <marcus@mh-development.info>
2016-05-15 19:22:47 +0200

    Removed obsolete control. THis now works with the audio controler

--- include/notewidget.h
+++ include/notewidget.h
@@ -39,7 +39,6 @@ protected:
     void mousePressEvent(QMouseEvent *event);
     void keyPressEvent(QKeyEvent *event);
     void mouseDoubleClickEvent(QMouseEvent *event);
-    void wheelEvent(QWheelEvent* event);
 
 private:
     static void loadSVG(QString file, QByteArray& target);
--- src/notewidget.cpp
+++ src/notewidget.cpp
@@ -300,13 +300,6 @@ void NoteWidget::mouseDoubleClickEvent(QMouseEvent *event) {
     }
 }
 
-void NoteWidget::wheelEvent(QWheelEvent *event) {
-    int steps = -event->delta()/8/15;
-
-    if (event->orientation() ==Qt::Vertical)
-        goToLine(std::min(std::max(0,currentLine+steps),_notes.size()-1));
-}
-
 void NoteWidget::mousePressEvent(QMouseEvent *event) {
     for (QRectF &r : texts.keys()) {
         if (r.contains(event->pos())) {
