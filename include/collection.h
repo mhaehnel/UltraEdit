e624fad97de68e756ef0b96def5062c251a21a7e
Marcus Haehnel <marcus@mh-development.info>
2016-05-22 11:57:20 +0200

    Made collections more versatile

--- include/collection.h
+++ include/collection.h
@@ -1,18 +1,21 @@
-#ifndef COLLECTION_H
-#define COLLECTION_H
+#pragma once
 
 #include <QString>
+#include <QDir>
+#include <QStringList>
+
+class Song;
 
 class Collection
 {
     QString basePath_;
     QString pathRule_;
     QString name_;
+    QStringList replacementTags;
 public:
     explicit Collection(QString name, QString basePath, QString pathRule);
     QString basePath() const { return basePath_; }
     QString name() const { return name_; }
     QString pathRule() const { return pathRule_; }
+    QFileInfo path(const Song* s, QString extension) const;
 };
-
-#endif // COLLECTION_H
--- src/collection.cpp
+++ src/collection.cpp
@@ -1,5 +1,25 @@
 #include "collection.h"
+#include <song.h>
 
 Collection::Collection(QString name, QString basePath, QString pathRule)
     : basePath_(basePath), pathRule_(pathRule), name_(name)
-{}
+{
+    QRegExp rex("\\$\\{([^\\}]*)\\}");
+    int pos;
+    while ((pos = rex.indexIn(pathRule_,pos)) != -1) {
+        if (!replacementTags.contains(rex.cap(1)))
+            replacementTags << rex.cap(1);
+        pos += rex.matchedLength();
+        qDebug() << rex.cap(1);
+    }
+    qDebug() << "Tags for pathRule" << pathRule_ << ":" << replacementTags;
+}
+
+QFileInfo Collection::path(const Song *s, QString extension) const {
+    QString res = pathRule_;
+    for (const QString& tag : replacementTags) {
+        res.replace(QString("${%1}").arg(tag),s->tag(tag));
+    }
+    res.replace("@{EXT}",extension);
+    return basePath_+'/'+res;
+}
